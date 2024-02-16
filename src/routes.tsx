af970439d4554be9ef4a7b2c3ad1d0fdcfd601bf
Fabianhenrick <fabianhenrick@gmail.com>
2024-02-15 22:53:10 -0300

    add layouts to aplication

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SingIn } from "./pages/auth/sing-in";
+import { AppLayout } from "./pages/_layouts/app";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/sing-in", element: <SingIn /> },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
+  },
+
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/sing-in", element: <SingIn /> }],
+  },
 ]);
