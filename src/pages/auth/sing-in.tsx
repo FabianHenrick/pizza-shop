9edd84e360d55eacdee0c79c79f316eaf3789f2f
Fabianhenrick <fabianhenrick@gmail.com>
2024-02-16 22:31:04 -0300

    Auth more modifications

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -4,7 +4,24 @@ export function SingIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SingIn</h1>;
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className=" flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            ;
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form action="" className="space-y-4">
+            <div className="space-y2"></div>
+            <button type="submit">Acessar Painel</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
