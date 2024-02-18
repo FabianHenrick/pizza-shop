c2be08c4f522fab5c88102a189a7283e2e7009cb
Fabianhenrick <fabianhenrick@gmail.com>
2024-02-18 12:44:51 -0300

    Sing-up Register Form

--- src/pages/auth/sing-up.tsx
+++ src/pages/auth/sing-up.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
+
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { z } from "zod";
 
 const singUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
   email: z.string().email(),
 });
 
@@ -50,12 +54,45 @@ export function SingUp() {
             className="space-y-4"
           >
             <div className="space-y2">
+              <Label htmlFor="email">Nome do Estabelecimento</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register("restaurantName")}
+              />
+            </div>
+
+            <div className="space-y2">
+              <Label htmlFor="email">Seu nome</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register("managerName")}
+              />
+            </div>
+
+            <div className="space-y2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="text" {...register("email")} />
             </div>
+
+            <div className="space-y2">
+              <Label htmlFor="email">Seu celular</Label>
+              <Input id="phone" type="tel" {...register("phone")} />
+            </div>
+
             <Button disabled={isSubmitting} className="w-full" type="submit">
               Finalizar Cadastro
             </Button>
+            <p className=" px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar, você concorda com os nossos{" "}
+              <a className="underline underline-offset-4">termos de serviço</a>{" "}
+              e{" "}
+              <a className="underline underline-offset-4">
+                politicas de privacidade
+              </a>
+              .
+            </p>
           </form>
         </div>
       </div>
