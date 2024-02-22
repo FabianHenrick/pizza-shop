0c28ac26363024879a16d4144c32791799225ef6
Fabianhenrick <fabianhenrick@gmail.com>
2024-02-22 20:43:33 -0300

    cards revenue and orders amount

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign } from "lucide-react";
 import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./dashbpard-revenue-card";
+import { MonthOrdersAmountCard } from "./month-orders-amount-card";
 
 export function Dashboard() {
   return (
@@ -9,25 +9,8 @@ export function Dashboard() {
       <div className=" flex flex-col gap-4">
         <h1 className="font text-3xl tracking-tighter"> Dashboard</h1>
         <div className=" grid grid-cols-4 gap-4">
-          <Card>
-            <CardHeader className=" flex  flex-row  items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-base font-semibold">
-                Receita Total (mês)
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent className="space-y-1">
-              <span className="text-2xl font-bold tracking-tight">
-                {" "}
-                R$1248,60
-              </span>
-              <p className="text-xs text-muted-foreground">
-                {" "}
-                <span className=" text-emerald-500">+2%</span> em relação ao mês
-                passado
-              </p>
-            </CardContent>
-          </Card>
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
         </div>
       </div>
     </>
