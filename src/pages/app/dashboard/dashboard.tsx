439cf4e26352b1430a06475ec99c494f4fa0ba3a
Fabianhenrick <fabianhenrick@gmail.com>
2024-02-28 22:14:10 -0300

    grid col fix

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -18,7 +18,7 @@ export function Dashboard() {
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
         </div>
-        <div className="grid-cools-9 grid gap-4">
+        <div className="grid grid-cols-9 gap-4">
           <RevenueChart />
           <PopularProductsChart />
         </div>
