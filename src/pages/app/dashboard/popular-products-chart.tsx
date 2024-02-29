e599014e4d90c1a1de91c11083bff7e4df8cb476
Fabianhenrick <fabianhenrick@gmail.com>
2024-02-28 22:10:45 -0300

    Chart Pie stylizatio, label view and more

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -26,6 +26,14 @@ const data = [
   { product: "Mussarela", amount: 45 },
 ];
 
+const COLORS = [
+  colors.sky["500"],
+  colors.amber["500"],
+  colors.violet["500"],
+  colors.emerald["500"],
+  colors.rose["500"],
+];
+
 export function PopularProductsChart() {
   return (
     <Card className="col-span-3">
@@ -52,7 +60,47 @@ export function PopularProductsChart() {
               outerRadius={86}
               innerRadius={64}
               strokeWidth={8}
-            ></Pie>
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180;
+                const radius = 12 + innerRadius + (outerRadius - innerRadius);
+                const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? "start" : "end"}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat("...")
+                      : data[index].product}{" "}
+                    ({value})
+                  </text>
+                );
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80"
+                  ></Cell>
+                );
+              })}
+            </Pie>
           </PieChart>
         </ResponsiveContainer>
       </CardContent>
