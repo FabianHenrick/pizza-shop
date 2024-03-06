f841bd4046aa8f1cf68b8463d750acce35b3cb46
Fabianhenrick <fabianhenrick@gmail.com>
2024-03-05 21:02:02 -0300

    Fix, Post requisition error(authenticate instead restaurants

--- src/api/register-restaurant.ts
+++ src/api/register-restaurant.ts
@@ -12,7 +12,7 @@ export async function registerRestaurant({
   managerName,
   phone,
 }: RegisterRestaurantBody) {
-  await api.post("/authenticate", {
+  await api.post("/restaurants", {
     restaurantName,
     email,
     managerName,
