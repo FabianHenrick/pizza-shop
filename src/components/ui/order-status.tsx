1e413a32e4a69b67e88299be093a501a8a34832f
Fabianhenrick <fabianhenrick@gmail.com>
2024-03-11 23:57:04 -0300

    pagination implementation

--- src/components/ui/order-status.tsx
+++ src/components/ui/order-status.tsx
@@ -20,23 +20,21 @@ const orderStatusMap: Record<OrderStatus, string> = {
 export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
-      <span className="h-2 w-2 rounded-full bg-slate-400">
-        {status === "pending" && (
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-        )}
+      {status === "pending" && (
+        <span className="h-2 w-2 rounded-full bg-slate-400" />
+      )}
 
-        {status === "canceled" && (
-          <span className="h-2 w-2 rounded-full bg-rose-400" />
-        )}
+      {status === "canceled" && (
+        <span className="h-2 w-2 rounded-full bg-rose-400" />
+      )}
 
-        {status === "delivered" && (
-          <span className="h-2 w-2 rounded-full bg-emerald-500" />
-        )}
+      {status === "delivered" && (
+        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+      )}
 
-        {["processing", "delivering"].includes(status) && (
-          <span className="h-2 w-2 rounded-full bg-amber-500" />
-        )}
-      </span>
+      {["processing", "delivering"].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      )}
       <span className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
       </span>
