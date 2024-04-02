d09a7b05eadc712db478267a341ebfbd0d159790
Fabianhenrick <fabianhenrick@gmail.com>
2024-04-01 23:32:31 -0300

    Mutations to order status

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, Variable, X } from "lucide-react";
+import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
 import { OrderStatus } from "@/components/ui/order-status";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { useState } from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { cancelOrder } from "@/api/cancel-order";
 import { GetOrdersResponse } from "@/api/get-orders";
+import { approveOrder } from "@/api/approve-order";
+import { deliverOrder } from "@/api/deliver-order";
+import { dispatchOrder } from "@/api/dispatch-order";
 
 export interface OrderTableRowProps {
   order: {
@@ -26,26 +29,51 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
 
   const queryClient = useQueryClient();
 
+  function updateOrderStatusOnCache(orderId: string, status: OrderStatus) {
+    const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+      queryKey: ["orders"],
+    });
+    ordersListCache.forEach(([cacheKey, cacheData]) => {
+      if (!cacheData) {
+        return;
+      }
+      queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+        ...cacheData,
+        orders: cacheData.orders.map((order) => {
+          if (order.orderId === orderId) {
+            return { ...order, status };
+          }
+          return order;
+        }),
+      });
+    });
+  }
+
   const { mutateAsync: cancelOrderFn } = useMutation({
     mutationFn: cancelOrder,
     onSuccess: (_, { orderId }) => {
-      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
-        queryKey: ["orders"],
-      });
-      ordersListCache.forEach(([cacheKey, cacheData]) => {
-        if (!cacheData) {
-          return;
-        }
-        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
-          ...cacheData,
-          orders: cacheData.orders.map((order) => {
-            if (order.orderId === orderId) {
-              return { ...order, status: "canceled" };
-            }
-            return order;
-          }),
-        });
-      });
+      updateOrderStatusOnCache(orderId, "canceled");
+    },
+  });
+
+  const { mutateAsync: approvedOrderFn } = useMutation({
+    mutationFn: approveOrder,
+    onSuccess: (_, { orderId }) => {
+      updateOrderStatusOnCache(orderId, "processing");
+    },
+  });
+
+  const { mutateAsync: deliverOrderFn } = useMutation({
+    mutationFn: deliverOrder,
+    onSuccess: (_, { orderId }) => {
+      updateOrderStatusOnCache(orderId, "delivered");
+    },
+  });
+
+  const { mutateAsync: dispatchOrderFn } = useMutation({
+    mutationFn: dispatchOrder,
+    onSuccess: (_, { orderId }) => {
+      updateOrderStatusOnCache(orderId, "delivering");
     },
   });
 
